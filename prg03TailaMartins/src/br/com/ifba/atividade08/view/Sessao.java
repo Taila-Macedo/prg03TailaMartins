1cbbe92e21eb52fa8ab493dc9ab58cb805ce795e
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-17 16:16:35 -0300

    Implementa a classe Sessão para criar e gerenciar tokens de autenticação

--- prg03TailaMartins/src/br/com/ifba/atividade08/view/Sessao.java
+++ prg03TailaMartins/src/br/com/ifba/atividade08/view/Sessao.java
@@ -9,5 +9,43 @@ package br.com.ifba.atividade08.view;
  * @author Taila
  */
 public class Sessao {
+    private Long id;
+    private Usuario usuario;
+    private String token;
+
+    public Sessao(Long id, Usuario usuario, String token){
+        this.id = id;
+        this.usuario = usuario;
+        this.token = token;
+        
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
+    public String toString(){
+        return "Sessao{id=" + id + ", usuario=" + usuario.getNomeUsuario() + ", token='" + token + "'}";
+    }
+        
     
 }
