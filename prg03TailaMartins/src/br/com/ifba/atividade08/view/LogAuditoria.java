cc084afe69d3af6d56633433e92ef20a953ca91f
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-17 16:34:22 -0300

    Adiciona a classe LogAuditoria para registrar logs de ações do usuário

--- prg03TailaMartins/src/br/com/ifba/atividade08/view/LogAuditoria.java
+++ prg03TailaMartins/src/br/com/ifba/atividade08/view/LogAuditoria.java
@@ -3,11 +3,69 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.ifba.atividade08.view;
-
+import java.time.LocalDateTime;
 /**
  *
  * @author Taila
  */
 public class LogAuditoria {
+    private Long id;
+    private Usuario usuario;
+    private String acao;
+    private LocalDateTime dataHora;
+    private String ip;
+    
+     public LogAuditoria(Long id, Usuario usuario, String acao, LocalDateTime dataHora, String ip){
+        this.acao = acao;
+        this.dataHora = dataHora;
+        this.id = id;
+        this.ip = ip;
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getAcao() {
+        return acao;
+    }
+
+    public void setAcao(String acao) {
+        this.acao = acao;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+    
+    public String toString(){
+        return "LogAuditoria{id=" + id + ", usuario=" + usuario.getNomeUsuario() +
+                ", acao='" + acao + "', dataHora=" + dataHora + ", ip='" + ip + "'}";
+    }
     
 }
