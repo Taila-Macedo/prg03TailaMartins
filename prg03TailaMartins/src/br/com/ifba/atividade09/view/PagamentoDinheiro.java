67a6ab2be6d0b2c4b42f3d65d0562788252f15c2
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-22 21:10:48 -0300

    implementa classe PagamentoDinheiro com 10% de desconto

--- prg03TailaMartins/src/br/com/ifba/atividade09/view/PagamentoDinheiro.java
+++ prg03TailaMartins/src/br/com/ifba/atividade09/view/PagamentoDinheiro.java
@@ -3,11 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.ifba.atividade09.view;
-
+import javax.swing.JTextArea;
 /**
  *
  * @author Taila
  */
-public class PagamentoDinheiro {
+public class PagamentoDinheiro implements Pagamento{
+    private double valor;
+
+    public PagamentoDinheiro(double valor) {
+        this.valor = valor;
+    }
     
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public double calcularTotal(double valor) {
+        valor = valor * 0.9;
+        return valor;
+    }
+
+    @Override
+    public void imprimirRecibo(JTextArea area) {
+    String texto = "";
+    texto += "=== RECIBO DINHEIRO ===\n";
+    texto += String.format("Valor original: R$ %.2f\n", valor);
+    texto += "Desconto de 10% aplicado!\n";
+    texto += String.format("Valor final: R$ %.2f\n", calcularTotal(valor));
+          
+    area.setText(texto);  
+    }
 }
