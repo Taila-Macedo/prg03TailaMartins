a8ac6cc83a01469976bde0744364ada78d97e747
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-22 21:12:10 -0300

    implementa classe PagamentoCartao com taxa de 5%

--- prg03TailaMartins/src/br/com/ifba/atividade09/view/PagamentoCartao.java
+++ prg03TailaMartins/src/br/com/ifba/atividade09/view/PagamentoCartao.java
@@ -3,11 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.ifba.atividade09.view;
-
+import javax.swing.JTextArea;
 /**
  *
  * @author Taila
  */
-public class PagamentoCartao {
+public class PagamentoCartao implements Pagamento{
+    private double valor;
+
+    public PagamentoCartao(double valor) {
+        this.valor = valor;
+    }
     
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public double calcularTotal(double valor) {
+       return valor * 1.05;
+    }
+
+    @Override
+    public void imprimirRecibo(JTextArea area) {
+        String texto = "";
+        texto += "=== RECIBO CARTÃO ===\n";
+        texto += String.format("Valor original: R$ %.2f\n", valor);
+        texto += "Taxa de 5% aplicada!\n";
+        texto += String.format("Valor final: R$ %.2f\n", calcularTotal(valor));
+        
+        area.setText(texto);
+    }
 }
