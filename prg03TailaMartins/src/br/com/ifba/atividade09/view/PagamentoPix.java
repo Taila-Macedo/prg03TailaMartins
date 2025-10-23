9ddfe80e94700f0cd70c7a86ca6b616414b9d0ec
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-22 21:13:14 -0300

    Implementa classe PagamentoPix com cashback de 2%

--- prg03TailaMartins/src/br/com/ifba/atividade09/view/PagamentoPix.java
+++ prg03TailaMartins/src/br/com/ifba/atividade09/view/PagamentoPix.java
@@ -3,11 +3,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.com.ifba.atividade09.view;
-
+import javax.swing.JTextArea;
 /**
  *
  * @author Taila
  */
-public class PagamentoPix {
+public class PagamentoPix implements Pagamento{
+    private double valor;
     
+    public PagamentoPix(double valor) {
+        this.valor = valor;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+    public double calcularTotal(double valor) {
+        valor = valor * 0.98;
+        return valor;
+    }
+    
+    
+    public void imprimirRecibo(javax.swing.JTextArea area){
+        String texto = "";
+        texto += "=== RECIBO PIX ===\n";
+        texto += String.format("Valor original: R$ %.2f\n", valor);
+        texto += String.format("Cashback de 2%%!\n");
+        texto += String.format("Valor final pago: R$ %.2f\n", calcularTotal(valor));
+        
+        area.setText(texto);
+    }
+
+    //@Override
+    //public void imprimirRecibo(JTextArea area) {
+      // String texto = String.format(
+        //    "=== RECIBO PIX ===\n\nDesconto de 2%% aplicado!\nValor final a pagar: R$ %.2f",
+          //  valor
+        //);
+       //area.setText(texto);
+    //}
 }
