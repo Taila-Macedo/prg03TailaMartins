0ad107e923fbee635c401d93378256750e24da1e
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-26 17:55:27 -0300

    Cria classe Principal

---
+++ prg03TailaMartins/src/br/com/ifba/atividade10/view/Principal.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade10.view;
+
+/**
+ *
+ * @author Taila
+ */
+public class Principal {
+    public static void main(String[] args){
+        Forma[] formas = {
+            new Circulo(6),
+            new Quadrado(4),
+            new Triangulo(6, 3),
+            new Esfera (4),
+            new Cubo (2),
+            new Piramide(4, 2)
+        };
+        
+        for (Forma f : formas) {
+           System.out.println("\n" + f.toString());
+           
+           if (f instanceof FormaBidimensional) {
+               FormaBidimensional forma2d = (FormaBidimensional) f;
+               System.out.printf("Area: %.2f\n", forma2d.obterArea());
+           }else
+               if(f instanceof FormaTridimensional) {
+                   FormaTridimensional forma3d = (FormaTridimensional) f;
+                   System.out.printf("Area: %.2f\n", forma3d.obterArea());
+                   System.out.printf("Volume: %.2f\n", forma3d.obterVolume());
+            }
+        }         
+    }
+    
+}
