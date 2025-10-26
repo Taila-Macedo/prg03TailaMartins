877a604cb4aa610efb855df0a4411bebedc1055c
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-26 16:30:01 -0300

    Cria a classe abstrata Forma

---
+++ prg03TailaMartins/src/br/com/ifba/atividade10/view/Forma.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade10.view;
+
+/**
+ *
+ * @author Taila
+ */
+public abstract class Forma {
+    public abstract String toString();
+}
