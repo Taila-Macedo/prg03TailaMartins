815ec64c56ee476165fe5d087e4e742efdad5b2b
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-26 17:04:19 -0300

    Adiciona classe Quadrado

---
+++ prg03TailaMartins/src/br/com/ifba/atividade10/view/Quadrado.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade10.view;
+
+/**
+ *
+ * @author Taila
+ */
+public class Quadrado extends FormaBidimensional {
+    private double lado;
+    
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    
+
+    @Override
+    public double obterArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Lado: " + lado;
+    }
+}
