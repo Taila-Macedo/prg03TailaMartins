f7387eba7b5603b1d1d2568dece4b37a0b007495
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-26 17:22:12 -0300

    Cria classe Esfera

---
+++ prg03TailaMartins/src/br/com/ifba/atividade10/view/Esfera.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade10.view;
+
+/**
+ *
+ * @author Taila
+ */
+public class Esfera extends FormaTridimensional {
+    private double raio;
+
+    public Esfera (double raio){
+        this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+    
+    @Override
+    public double obterArea() {
+        return 4 * Math.PI * raio * raio;
+    }
+
+    @Override
+    public double obterVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
+    }
+
+    @Override
+    public String toString() {
+        return "-------Esfera------\n" +
+                "Raio: " + raio;
+    }
+    
+    
+}
