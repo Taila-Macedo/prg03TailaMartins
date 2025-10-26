1a36db086fbc2ffba9a46944c88f92b31aa1d97f
Taila-Macedo <20242ireads0011@ifba.edu.br>
2025-10-26 16:52:17 -0300

    Implementa Círculo

---
+++ prg03TailaMartins/src/br/com/ifba/atividade10/view/Circulo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade10.view;
+
+/**
+ *
+ * @author Taila
+ */
+public class Circulo extends FormaBidimensional {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+    
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double obterArea() {
+        return Math.PI * raio * raio;
+    }
+
+    @Override
+    public String toString() {
+        return "Círculo de raio " + raio;
+    }
+}
